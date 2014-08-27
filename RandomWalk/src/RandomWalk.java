89ebe3d74f10c4134c28d2c2c3d6de9f4ff18ee9
Andy C <acobley@computing.dundee.ac.uk>
2014-08-27 18:15:26 +0100

    using countdown

--- RandomWalk/src/RandomWalk.java
+++ RandomWalk/src/RandomWalk.java
@@ -1,44 +1,39 @@
 import java.util.Date;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 
 public class RandomWalk {
-	static int ActualNoWalkers = 6000;
+	static int ActualNoWalkers = 6;
 	static int numberInPool=1000;
 		
-	static Executor pool = Executors.newFixedThreadPool(numberInPool);
-	public static void main(String[] args) {
+	//static ExecutorService pool = Executors.newFixedThreadPool(numberInPool);
+	public static void main(String[] args) throws InterruptedException {
 		long now=new Date().getTime();
 		System.out.println("Start " +now );
-		//final Walker Walkers[]=new Walker[ActualNoWalkers];
-		for (int WalkerCount=0; WalkerCount<ActualNoWalkers; WalkerCount++){	//Walkers count from zero, so we drop out of the loop at ActualNoWalkers - 1	
-			//if (Walkers[WalkerCount] == null) {
-				
-				Runnable r = new Runnable()
-	            {
-	               @Override
-	               public void run()
-	               {
-	            	   Walker walker=new Walker();
-	            	   walker.walk();
-	               }
-	            };
-	            pool.execute(r);
-			//}
-			
-			//Walkers[WalkerCount].walk();
-		}
-		//Now they have all moved
 		double TotalDistance=0.0;
-		/*
-		for (int WalkerCount=0; WalkerCount<ActualNoWalkers; WalkerCount++){	//Walkers count from zero, so we drop out of the loop at ActualNoWalkers - 1	
-		    double Distance=Walkers[WalkerCount].distanceFromCenter();
-			
-			TotalDistance=TotalDistance+Distance;
-		}*/
+		CountDownLatch startSignal = new CountDownLatch(1);
+	    CountDownLatch doneSignal = new CountDownLatch(numberInPool);
+        double distances[]=new double[numberInPool];
+				for (int i=0;i<numberInPool;i++){
+					new Thread(new Walker(startSignal, doneSignal,i,distances)).start();
+				}
+					startSignal.countDown();
+					doneSignal.await();
+
+
+		for (int i=0;i<numberInPool;i++){
+			System.out.println(distances[i]);
+			TotalDistance=TotalDistance+distances[i];
+		}
 		long elapsed=new Date().getTime()- now;
-		System.out.println("Average Distance="+TotalDistance/(double)ActualNoWalkers+" : "+elapsed);
+		System.out.println("Average Distance="+TotalDistance/(double)(numberInPool)+" : "+elapsed);
+
 	}
 
 }
--- RandomWalk/src/Walker.java
+++ RandomWalk/src/Walker.java
@@ -1,14 +1,24 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
 
-public class Walker {
+
+public class Walker implements Runnable{
 
 	private double X=0;
 	private double Y=0;
 
 	private double TotalDistance=0;
-	static int ActualNoSteps = 500000;	// Actual number of steps for this run. Up to 5,000 and 22,000 walkers seems to work fine.
-
-	public Walker(){ //Constructor
-		
+	static int ActualNoSteps = 500;	// Actual number of steps for this run. Up to 5,000 and 22,000 walkers seems to work fine.
+    int i=-1;
+    private final CountDownLatch startSignal;
+    private final CountDownLatch doneSignal;
+    static int numberInPool=100;
+    double distances[]=new double[numberInPool];
+	Walker(CountDownLatch startSignal, CountDownLatch doneSignal,int i,double distances[]){ //Constructor
+		this.startSignal = startSignal;
+	      this.doneSignal = doneSignal;
+	      this.i=i;
+	      this.distances=distances;
 	}
 	
 	public double getX(){
@@ -35,6 +45,22 @@ public class Walker {
 	public double distanceFromCenter(){
 		double distance= Math.sqrt((double)(X*X+Y*Y));
 		return distance;
+	}
+	
+	public void run(){
+		try{
+		String name = Thread.currentThread().getName();
+		startSignal.await();
+		System.out.println("Starting Walker "+name +" : "+i);
+		walk();
+		System.out.println("Stopping Walker"+name+" : "+i+" : "+distanceFromCenter());
+		this.distances[i]=distanceFromCenter();
+		doneSignal.countDown();
+		}
+		catch (InterruptedException ie)
+	      {
+	         System.out.println("interrupted");
+	      }
 	}
 	
 }
